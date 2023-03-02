ad50156fb9af1cc669b7f1c444f7fd2f6c64883e
Arjun Bhadra <abhadra@adobe.com>
2023-03-02 15:04:09 -0800

    Added dummy test to avoid functionalTest failures

---
+++ code/edgemedia/src/androidTest/java/com/adobe/marketing/mobile/edge/media/internal/FunctionalTest.java
@@ -0,0 +1,10 @@
+package com.adobe.marketing.mobile.edge.media.internal;
+
+import org.junit.Test;
+
+public class FunctionalTest {
+    @Test
+    public void test() {
+        // dummy test
+    }
+}
