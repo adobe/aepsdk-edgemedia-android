a40f7a2c6f319e5b2afa2364734482ca7ee595a7
Kevin Lind <lind@adobe.com>
2023-03-06 10:45:55 -0800

    Format test app code

--- code/testapp/src/main/java/com/adobe/mediaanalyticstestapp/MediaActivity.java
+++ code/testapp/src/main/java/com/adobe/mediaanalyticstestapp/MediaActivity.java
@@ -1,104 +1,98 @@
-///*************************************************************************
-// * ADOBE CONFIDENTIAL
-// * ___________________
-// *
-// * Copyright 2021 Adobe
-// * All Rights Reserved.
-// *
-// * NOTICE: All information contained herein is, and remains
-// * the property of Adobe and its suppliers, if any. The intellectual
-// * and technical concepts contained herein are proprietary to Adobe
-// * and its suppliers and are protected by all applicable intellectual
-// * property laws, including trade secret and copyright laws.
-// * Dissemination of this information or reproduction of this material
-// * is strictly forbidden unless prior written permission is obtained
-// * from Adobe.
-// **************************************************************************/
+/*
+  Copyright 2021 Adobe. All rights reserved.
+  This file is licensed to you under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License. You may obtain a copy
+  of the License at http://www.apache.org/licenses/LICENSE-2.0
+  Unless required by applicable law or agreed to in writing, software distributed under
+  the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+  OF ANY KIND, either express or implied. See the License for the specific language
+  governing permissions and limitations under the License.
+*/
+
 package com.adobe.mediaanalyticstestapp;
 
-import android.os.Bundle;
 import android.app.Activity;
 import android.net.Uri;
+import android.os.Bundle;
 import android.view.View;
-
 import com.adobe.mediaanalyticstestapp.analytics.VideoAnalyticsProvider;
 import com.adobe.mediaanalyticstestapp.player.PlayerEvent;
 import com.adobe.mediaanalyticstestapp.player.VideoPlayer;
-
 import java.util.Observable;
 import java.util.Observer;
 
-
 public class MediaActivity extends Activity implements Observer {
-	private VideoPlayer _player;
-	private VideoAnalyticsProvider _analyticsProvider;
+    private VideoPlayer _player;
+    private VideoAnalyticsProvider _analyticsProvider;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activitymedia);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activitymedia);
 
-		// Create the VideoPlayer instance.
-		_player = new VideoPlayer(this);
+        // Create the VideoPlayer instance.
+        _player = new VideoPlayer(this);
 
-		_player.addObserver(this);
+        _player.addObserver(this);
 
-		// Create the VideoAnalyticsProvider instance and
-		// attach it to the VideoPlayer instance.
-		_analyticsProvider = new VideoAnalyticsProvider(_player);
+        // Create the VideoAnalyticsProvider instance and
+        // attach it to the VideoPlayer instance.
+        _analyticsProvider = new VideoAnalyticsProvider(_player);
 
-		// Load the main video content.
-		Uri uri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.video);
-		_player.loadContent(uri);
-	}
+        // Load the main video content.
+        Uri uri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.video);
+        _player.loadContent(uri);
+    }
 
-	@Override
-	protected void onDestroy() {
-		_analyticsProvider.destroy();
-		_analyticsProvider = null;
-		_player = null;
+    @Override
+    protected void onDestroy() {
+        _analyticsProvider.destroy();
+        _analyticsProvider = null;
+        _player = null;
 
-		super.onDestroy();
-	}
+        super.onDestroy();
+    }
 
-	@Override
-	public void update(Observable observable, Object o) {
-		PlayerEvent playerEvent = (PlayerEvent) o;
+    @Override
+    public void update(Observable observable, Object o) {
+        PlayerEvent playerEvent = (PlayerEvent) o;
 
-		switch (playerEvent) {
-			case AD_START:
-				_onEnterAd();
-				break;
+        switch (playerEvent) {
+            case AD_START:
+                _onEnterAd();
+                break;
 
-			case AD_COMPLETE:
-				_onExitAd();
-				break;
+            case AD_COMPLETE:
+                _onExitAd();
+                break;
 
-			case SEEK_COMPLETE:
-				if (_player.getAdInfo() == null) {
-					// The user seeked outside the ad.
-					_onExitAd();
-				}
+            case SEEK_COMPLETE:
+                if (_player.getAdInfo() == null) {
+                    // The user seeked outside the ad.
+                    _onExitAd();
+                }
 
-				break;
-		}
-	}
+                break;
+        }
+    }
 
-	private void _onEnterAd() {
-		runOnUiThread(new Runnable() {
-			@Override
-			public void run() {
-				findViewById(R.id.adOverlayView).setVisibility(View.VISIBLE);
-			}
-		});
-	}
+    private void _onEnterAd() {
+        runOnUiThread(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        findViewById(R.id.adOverlayView).setVisibility(View.VISIBLE);
+                    }
+                });
+    }
 
-	private void _onExitAd() {
-		runOnUiThread(new Runnable() {
-			@Override
-			public void run() {
-				findViewById(R.id.adOverlayView).setVisibility(View.INVISIBLE);
-			}
-		});
-	}
+    private void _onExitAd() {
+        runOnUiThread(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        findViewById(R.id.adOverlayView).setVisibility(View.INVISIBLE);
+                    }
+                });
+    }
 }
--- code/testapp/src/main/java/com/adobe/mediaanalyticstestapp/player/PlayerEvent.java
+++ code/testapp/src/main/java/com/adobe/mediaanalyticstestapp/player/PlayerEvent.java
@@ -1,47 +1,41 @@
-/*************************************************************************
- * ADOBE CONFIDENTIAL
- * ___________________
- *
- * Copyright 2021 Adobe
- * All Rights Reserved.
- *
- * NOTICE: All information contained herein is, and remains
- * the property of Adobe and its suppliers, if any. The intellectual
- * and technical concepts contained herein are proprietary to Adobe
- * and its suppliers and are protected by all applicable intellectual
- * property laws, including trade secret and copyright laws.
- * Dissemination of this information or reproduction of this material
- * is strictly forbidden unless prior written permission is obtained
- * from Adobe.
- **************************************************************************/
+/*
+  Copyright 2021 Adobe. All rights reserved.
+  This file is licensed to you under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License. You may obtain a copy
+  of the License at http://www.apache.org/licenses/LICENSE-2.0
+  Unless required by applicable law or agreed to in writing, software distributed under
+  the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+  OF ANY KIND, either express or implied. See the License for the specific language
+  governing permissions and limitations under the License.
+*/
 
 package com.adobe.mediaanalyticstestapp.player;
 
 public enum PlayerEvent {
-	VIDEO_LOAD("video_load"),
-	VIDEO_UNLOAD("video_unload"),
-	PLAY("play"),
-	PAUSE("pause"),
-	SEEK_START("seek_start"),
-	SEEK_COMPLETE("seek_complete"),
-	BUFFER_START("buffer_start"),
-	BUFFER_COMPLETE("buffer_complete"),
-	AD_START("ad_start"),
-	AD_COMPLETE("ad_complete"),
-	CHAPTER_START("chapter_start"),
-	CHAPTER_COMPLETE("chapter_complete"),
-	COMPLETE("complete"),
-	PLAYHEAD_UPDATE("playhead_update"),
-	PLAYER_STATE_MUTE_START("player_state_mute_start"),
-	PLAYER_STATE_MUTE_END("player_state_mute_end");
+    VIDEO_LOAD("video_load"),
+    VIDEO_UNLOAD("video_unload"),
+    PLAY("play"),
+    PAUSE("pause"),
+    SEEK_START("seek_start"),
+    SEEK_COMPLETE("seek_complete"),
+    BUFFER_START("buffer_start"),
+    BUFFER_COMPLETE("buffer_complete"),
+    AD_START("ad_start"),
+    AD_COMPLETE("ad_complete"),
+    CHAPTER_START("chapter_start"),
+    CHAPTER_COMPLETE("chapter_complete"),
+    COMPLETE("complete"),
+    PLAYHEAD_UPDATE("playhead_update"),
+    PLAYER_STATE_MUTE_START("player_state_mute_start"),
+    PLAYER_STATE_MUTE_END("player_state_mute_end");
 
-	private final String _type;
+    private final String _type;
 
-	PlayerEvent(String type) {
-		_type = type;
-	}
+    PlayerEvent(String type) {
+        _type = type;
+    }
 
-	public String getType() {
-		return _type;
-	}
+    public String getType() {
+        return _type;
+    }
 }
