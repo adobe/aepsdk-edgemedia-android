e1422608fe73235a54d15cd2d7e5a1718bd3282b
Kevin Lind <lind@adobe.com>
2023-05-16 11:51:37 -0700

    Fix AudioMetadataKeys.ALBUM name

--- code/edgemedia/src/phone/java/com/adobe/marketing/mobile/edge/media/MediaConstants.java
+++ code/edgemedia/src/phone/java/com/adobe/marketing/mobile/edge/media/MediaConstants.java
@@ -42,7 +42,7 @@ public class MediaConstants {
 
     /** These constant strings define standard audio metadata keys. */
     public static final class AudioMetadataKeys {
-        public static final String ALBUM = "mediaCollection.sessionDetails.album";
+        public static final String ALBUM = "a.media.album";
         public static final String ARTIST = "a.media.artist";
         public static final String AUTHOR = "a.media.author";
         public static final String LABEL = "a.media.label";
