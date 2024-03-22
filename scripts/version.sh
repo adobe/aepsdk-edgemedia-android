73665f105327dd051866e48219f0367ef10c0baa
praveek <praveek@users.noreply.github.com>
2024-03-22 16:42:49 -0700

    Fix path again

--- scripts/version.sh
+++ scripts/version.sh
@@ -9,7 +9,7 @@ LINE="==========================================================================
 VERSION_REGEX="[0-9]+\.[0-9]+\.[0-9]+"
 
 GRADLE_PROPERTIES_FILE=$ROOT_DIR"/code/gradle.properties"
-CONSTANTS_FILE=$ROOT_DIR"/code/edgemedia/src/phone/java/com/adobe/marketing/mobile/edge/Media.java"
+CONSTANTS_FILE=$ROOT_DIR"/code/edgemedia/src/phone/java/com/adobe/marketing/mobile/edge/media/Media.java"
 # Java files
 EXTENSION_VERSION_REGEX="^.*String EXTENSION_VERSION *= *"
 # Kotlin files
